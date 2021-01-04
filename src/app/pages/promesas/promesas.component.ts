6b15b21386601a5f4bf3953dedda85b28d1489f9
Miguel-Pariente <miguelpl32@hotmail.com>
2021-01-04 20:39:53 +0100

    Promesas Observable Rxjs

---
+++ src/app/pages/promesas/promesas.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promesas',
+  templateUrl: './promesas.component.html',
+  styleUrls: ['./promesas.component.css']
+})
+export class PromesasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.getUsuarios().then(usuarios => {
+      console.log(usuarios);
+    });
+
+    //   const promesa = new Promise((resolve, reject) => {
+
+    //     if (false) {
+    //       resolve('Hola Mundo');
+    //     } else {
+    //       reject('Algo salio mal');
+    //     }
+
+    //   });
+
+    //   promesa.then((mensaje) => {
+    //     console.log(mensaje);
+    //   })
+    //     .catch(error => console.log('Error en la promesa', error));
+
+    //   console.log('Fin del Init');
+  }
+
+  getUsuarios() {
+
+    return new Promise((resolve, reject) => {
+
+      fetch('https://reqres.in/api/users')
+        .then(resp => resp.json())
+        .then(body => resolve(body.data));
+
+    });
+
+
+  }
+
+}
